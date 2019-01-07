22d1170248a913203aa6be8956565f88a7ba8da8
mibo <mibo@mirb.de>
2019-01-07 21:14:00 +0100

    Set next SNAPSHOT

--- scripts/Docker/dockerBuild.sh
+++ scripts/Docker/dockerBuild.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # RELEASES
-docker build --build-arg JAR_FILE=build/libs/miftp-0.2.0.jar --build-arg HTTP_PORT=8081 -t mibo/miftp -t mibo/miftp:0.2.0 -f ./Dockerfile ../..
+#docker build --build-arg JAR_FILE=build/libs/miftp-0.2.0.jar --build-arg HTTP_PORT=8081 -t mibo/miftp -t mibo/miftp:0.2.0 -f ./Dockerfile ../..
 # SNAPSHOTs
-#docker build --build-arg JAR_FILE=build/libs/miftp-0.2.0-SNAPSHOT.jar --build-arg HTTP_PORT=8081 -t mibo/miftp:sn -f ./Dockerfile ../..
+docker build --build-arg JAR_FILE=build/libs/miftp-0.3.0-SNAPSHOT.jar --build-arg HTTP_PORT=8081 -t mibo/miftp:sn -f ./Dockerfile ../..
--- scripts/rpiDocker/dockerBuild.sh
+++ scripts/rpiDocker/dockerBuild.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Build command for Releases
-docker build --build-arg JAR_FILE=build/libs/miftp-0.2.0.jar --build-arg HTTP_PORT=8081 -t mibo/rpi-miftp -t mibo/rpi-miftp:0.2.0 -f ./Dockerfile ../..
+#docker build --build-arg JAR_FILE=build/libs/miftp-0.2.0.jar --build-arg HTTP_PORT=8081 -t mibo/rpi-miftp -t mibo/rpi-miftp:0.2.0 -f ./Dockerfile ../..
 # Build command for SNAPSHOTs
-#docker build --build-arg JAR_FILE=build/libs/miftp-0.2.0-SNAPSHOT.jar --build-arg HTTP_PORT=8081 -t mibo/rpi-miftp:sn -f ./Dockerfile ../..
+docker build --build-arg JAR_FILE=build/libs/miftp-0.3.0-SNAPSHOT.jar --build-arg HTTP_PORT=8081 -t mibo/rpi-miftp:sn -f ./Dockerfile ../..
