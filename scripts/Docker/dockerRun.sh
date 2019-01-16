aceb51200225873bc4dbb51ec526742515dcff6c
mibo <mibo@mirb.de>
2019-01-16 21:57:10 +0100

    Fixed (and commented) docker config(s)

--- scripts/Docker/dockerRun.sh
+++ scripts/Docker/dockerRun.sh
@@ -5,17 +5,20 @@ if [[ -n $1 ]]; then
 fi
 echo "Start with $IMAGE"
 
+# can not be configured here because of build/Dockerfile settings
+#  -e SERVER_PORT=8080 \
+#  -e HTTP_PORT=8080 \
+#  -e MIFTP_FTP_PORT=50021 \
+
 docker run --rm \
-  -p 8081:8080 -p 50021:50021 \
+  -p 8081:8081 -p 50021:50021 \
   -p 50100-50200:50100-50200 \
   -v "$(pwd)"/../keystore.jks:/config/keystore.jks \
   -e MIFTP_KEYSTORE_NAME=config/keystore.jks \
   -e MIFTP_KEYSTORE_PASSWORD=password \
-  -e SERVER_PORT=8080 \
   -e MIFTP_USER=miftp \
   -e MIFTP_PASSWORD="{bcrypt}\$2a\$10\$5SyjnpMano4Z3LGbWQC9W.ySSsheBZI.7uufzpJ4uKokBGfd.uHau" \
-  -e MIFTP_FTP_PORT=50021 \
-  -e MIFTP_FTP_USER=ftp  \
+  -e MIFTP_FTP_USER=ftp \
   -e MIFTP_FTP_PASSWORD=ftp \
   -e MIFTP_FTP_MAXFILES=30 \
   $IMAGE
