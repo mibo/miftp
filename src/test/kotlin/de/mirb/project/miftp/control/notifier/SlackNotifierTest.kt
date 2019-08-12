67db21f4c8afe13d3a9328eefb532823a84507e6
mibo <mibo@mirb.de>
2019-08-12 19:52:10 +0200

    Fixed tests

--- src/test/kotlin/de/mirb/project/miftp/control/notifier/SlackNotifierTest.kt
+++ src/test/kotlin/de/mirb/project/miftp/control/notifier/SlackNotifierTest.kt
@@ -9,10 +9,12 @@ import java.lang.IllegalArgumentException
 
 class SlackNotifierTest {
 
+  val mandatoryInitParameters = mapOf("slack_webhook_url" to "http://")
+
   @Test
   fun initFilterEmpty() {
     val notifier = SlackNotifier()
-    notifier.init(mapOf("url" to "http://"))
+    notifier.init(mapOf("slack_webhook_url" to "http://"))
     //val contains = notifier.filter.containsAll(listOf(FileSystemEvent.EventType.CREATED, FileSystemEvent.EventType.DELETED))
     assertThat(notifier.filter, empty())
   }
@@ -20,7 +22,7 @@ class SlackNotifierTest {
   @Test
   fun initFilterSuccess() {
     val notifier = SlackNotifier()
-    notifier.init(mapOf("filter" to "CREATED, DELETED"))
+    notifier.init(mandatoryInitParameters.plus("event_filter" to "CREATED, DELETED"))
     //val contains = notifier.filter.containsAll(listOf(FileSystemEvent.EventType.CREATED, FileSystemEvent.EventType.DELETED))
     assertThat(notifier.filter, contains(FileSystemEvent.EventType.CREATED, FileSystemEvent.EventType.DELETED))
     assertThat(notifier.filter, not(contains(FileSystemEvent.EventType.MODIFIED)))
@@ -30,7 +32,7 @@ class SlackNotifierTest {
   fun initFilterSuccessInvalid() {
     val notifier = SlackNotifier()
     try {
-      notifier.init(mapOf("filter" to "CREATED, UNKNOWN, DELETED"))
+      notifier.init(mandatoryInitParameters.plus("event_filter" to "CREATED, UNKNOWN, DELETED"))
       fail("Expected exception was not thrown")
     } catch (e: IllegalArgumentException) {
       assertThat(e.message, endsWith("No enum constant de.mirb.project.miftp.fs.listener.FileSystemEvent.EventType.UNKNOWN"))
