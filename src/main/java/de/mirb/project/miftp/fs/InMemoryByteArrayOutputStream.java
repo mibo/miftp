afcccb7a06b8d47c7bda34c5c3dcd95546133bf6
mibo <mibo@mirb.de>
2019-08-14 21:25:05 +0200

    Improved CREATE notifications handling

--- src/main/java/de/mirb/project/miftp/fs/InMemoryByteArrayOutputStream.java
+++ src/main/java/de/mirb/project/miftp/fs/InMemoryByteArrayOutputStream.java
@@ -3,7 +3,12 @@ package de.mirb.project.miftp.fs;
 import java.io.ByteArrayOutputStream;
 
 public class InMemoryByteArrayOutputStream extends ByteArrayOutputStream {
+  private final InMemoryFtpFile file;
   private boolean closed = false;
+
+  public InMemoryByteArrayOutputStream(InMemoryFtpFile file) {
+    this.file = file;
+  }
 
   public boolean isClosed() {
     return closed;
@@ -11,7 +16,7 @@ public class InMemoryByteArrayOutputStream extends ByteArrayOutputStream {
 
   @Override
   public void close() {
-//    super.close();
     closed = true;
+    file.uploadFinished();
   }
 }
--- src/main/java/de/mirb/project/miftp/fs/InMemoryFtpFile.java
+++ src/main/java/de/mirb/project/miftp/fs/InMemoryFtpFile.java
@@ -1,8 +1,14 @@
 package de.mirb.project.miftp.fs;
 
+import de.mirb.project.miftp.fs.listener.FileSystemEvent;
+
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * Created by mibo on 21.04.17.
@@ -11,6 +17,7 @@ public class InMemoryFtpFile extends InMemoryFtpPath {
 
   private InMemoryByteArrayOutputStream bout;
   private byte[] content;
+  private boolean uploadFinished;
 
   public InMemoryFtpFile(InMemoryFsView view, InMemoryFtpDir parentDir, String name) {
     super(view, parentDir, name);
@@ -52,6 +59,7 @@ public class InMemoryFtpFile extends InMemoryFtpPath {
 
   private byte[] getContent() {
     synchronized (name) {
+      waitForCondition(3, SECONDS, 3, this::isUploadOngoing);
       if(isFlushed()) {
         if(content == null) {
           content = bout.toByteArray();
@@ -63,17 +71,36 @@ public class InMemoryFtpFile extends InMemoryFtpPath {
     return content;
   }
 
+  private void waitForCondition(long timeValue, TimeUnit timeUnit, int retries, Supplier<Boolean> condition) {
+    while(retries-- > 0 && condition.get()) {
+      try {
+        Thread.sleep(timeUnit.toMillis(timeValue));
+      } catch (InterruptedException e) {
+        // should never happen
+        Thread.currentThread().interrupt();
+      }
+    }
+  }
+
   @Override
   public OutputStream createOutputStream(long l) {
-    bout = new InMemoryByteArrayOutputStream();
+    bout = new InMemoryByteArrayOutputStream(this);
     content = null;
     lastModified = System.currentTimeMillis();
     return bout;
   }
 
+  public void uploadFinished() {
+    uploadFinished = true;
+    fsView.updateListener(this, FileSystemEvent.EventType.CREATED);
+  }
+
+  private boolean isUploadOngoing() {
+    return !isFlushed();
+  }
 
   public boolean isFlushed() {
-    if(bout == null) {
+    if(bout == null || !uploadFinished) {
       return false;
     } 
     return bout.isClosed();
