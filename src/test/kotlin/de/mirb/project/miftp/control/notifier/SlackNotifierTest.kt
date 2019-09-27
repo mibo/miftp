e8324ad55704c574a4844c39f74455c59ab5e7dc
mibo <mibo@mirb.de>
2019-09-27 20:24:37 +0200

    Added mockk and fixed tests

--- src/test/kotlin/de/mirb/project/miftp/control/notifier/SlackNotifierTest.kt
+++ src/test/kotlin/de/mirb/project/miftp/control/notifier/SlackNotifierTest.kt
@@ -1,6 +1,8 @@
 package de.mirb.project.miftp.control.notifier
 
+import de.mirb.project.miftp.control.FtpFileStore
 import de.mirb.project.miftp.fs.listener.FileSystemEvent
+import io.mockk.mockk
 import org.hamcrest.Matchers.*
 import org.junit.Assert.assertThat
 import org.junit.Assert.fail
@@ -13,7 +15,7 @@ class SlackNotifierTest {
   @Test
   fun initFilterEmpty() {
     val notifier = SlackNotifier()
-    notifier.init(mapOf("slack_webhook_url" to "http://"))
+    notifier.init(mapOf("slack_webhook_url" to "http://"), mockk<FtpFileStore>())
     //val contains = notifier.filter.containsAll(listOf(FileSystemEvent.EventType.CREATED, FileSystemEvent.EventType.DELETED))
     assertThat(notifier.filter, empty())
   }
@@ -21,7 +23,7 @@ class SlackNotifierTest {
   @Test
   fun initFilterSuccess() {
     val notifier = SlackNotifier()
-    notifier.init(mandatoryInitParameters.plus("event_filter" to "CREATED, DELETED"))
+    notifier.init(mandatoryInitParameters.plus("event_filter" to "CREATED, DELETED"), mockk<FtpFileStore>())
     //val contains = notifier.filter.containsAll(listOf(FileSystemEvent.EventType.CREATED, FileSystemEvent.EventType.DELETED))
     assertThat(notifier.filter, contains(FileSystemEvent.EventType.CREATED, FileSystemEvent.EventType.DELETED))
     assertThat(notifier.filter, not(contains(FileSystemEvent.EventType.MODIFIED)))
@@ -31,7 +33,8 @@ class SlackNotifierTest {
   fun initFilterSuccessInvalid() {
     val notifier = SlackNotifier()
     try {
-      notifier.init(mandatoryInitParameters.plus("event_filter" to "CREATED, UNKNOWN, DELETED"))
+      notifier.init(mandatoryInitParameters.plus("event_filter" to "CREATED, UNKNOWN, DELETED"),
+              mockk<FtpFileStore>())
       fail("Expected exception was not thrown")
     } catch (e: IllegalArgumentException) {
       assertThat(e.message, endsWith("No enum constant de.mirb.project.miftp.fs.listener.FileSystemEvent.EventType.UNKNOWN"))
